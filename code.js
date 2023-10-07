7ccd811c69c75f90018e0f37b49d5d6b55dbaa37
chirrylun <134626109+chirrylun@users.noreply.github.com>
2023-10-07 09:02:48 -0700

    Update

--- code.js
+++ code.js
@@ -35,3 +35,43 @@ function toggleShawContent() {
 
 // Attach the click event listener to the "shaw" element
 _f1('shaw').onclick = toggleShawContent;
+
+function _f1(id) {
+    return document.getElementById(id)
+}
+
+function toggleShawContent() {
+    let div = _f1('maindiv-workshop');
+    let shaw = _f1('shaw');
+
+    if (div.className === 'cls1') {
+        div.className = '';
+        shaw.innerHTML = 'Show more...'
+    } else {
+        div.className = 'cls1';
+        shaw.innerHTML = 'Show less...'
+    }
+}
+
+// Attach the click event listener to the "shaw" element
+_f1('shaw').onclick = toggleShawContent;
+
+function _f2(id) {
+    return document.getElementById(id)
+}
+
+function toggleShiwContent() {
+    let div = _f2('maindiv-research');
+    let shiw = _f2('shiw');
+
+    if (div.className === 'cls2') {
+        div.className = '';
+        shiw.innerHTML = 'Show more...'
+    } else {
+        div.className = 'cls2';
+        shiw.innerHTML = 'Show less...'
+    }
+}
+
+// Attach the click event listener to the "shaw" element
+_f1('shiw').onclick = toggleShiwContent;
